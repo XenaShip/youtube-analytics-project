677f9eb464f4c7c8e4abd83a55175a1bf2109242
Xena Ship <shipxena@gmail.com>
2023-03-23 10:56:50 +0300

    homework3

--- src/channel.py
+++ src/channel.py
@@ -41,3 +41,35 @@ class Channel:
                 'video_count': self.video_count,
                 'view_count': self.view_count
             }, fh)
+
+
+    def __str__(self):
+        return f"'{self.title} ({self.url})'"
+
+
+    def __add__(self, other):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+
+    def __sub__(self, other):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __gt__(self, other):
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __lt__(self, other):
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __le__(self, other):
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
+    def __eq__(self, other):
+        return int(self.subscriber_count) == int(other.subscriber_count)
+
+    def __ne__(self, other):
+        return int(self.subscriber_count) != int(other.subscriber_count)
+
+    def __ge__(self, other):
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+
